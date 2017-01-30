a58b0ad708eeea896e0a5d1dfaf43cde52684dec
gabsfachinotti <gabsfachinotti@gmail.com>
2017-01-30 13:30:25 -0300

    Modelo Inicial

---
+++ SistemaRentabilidad/Models/Sheet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaRentabilidad.Models
+{
+    public class Sheet
+    {
+        public int IdSheet { get; set; }
+
+        public string SheetDescription { get; set; }
+
+        public decimal Rode { get; set; }
+
+        public bool Type { get; set; }
+
+        public string Comments { get; set; }
+
+        public int idWorkSheet { get; set; } //Clave Foránea
+
+        public virtual Worksheet WorkSheet { get; set; }
+    }
+}
---
+++ SistemaRentabilidad/Models/Worksheet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaRentabilidad.Models
+{
+    public class Worksheet
+    {
+        public int IdWorksheet { get; set; }
+
+        public string WorksheetDescription { get; set; }
+
+        public decimal TotalAmount { get; set; }
+
+        public string Comments { get; set; }   
+        
+        public virtual ICollection<Sheet> Sheets { get; set; }     
+    }
+}
