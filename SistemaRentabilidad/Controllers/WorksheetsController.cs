34492cfada7fcd26f9d59704a5216da02fbd3482
mln6666 <poncemarioleo@gmail.com>
2017-02-20 08:24:09 -0300

    +En las sugerencias se muestran solo las sugerencias del tipo correspondiente a la tabla en la que se encuentre (ingreso/costo/gasto/otro) ***En la Nueva entrada se siguen mostrando todas las sugerencias

--- SistemaRentabilidad/Controllers/WorksheetsController.cs
+++ SistemaRentabilidad/Controllers/WorksheetsController.cs
@@ -18,7 +18,7 @@ namespace SistemaRentabilidad.Controllers
     [Authorize(Users = "urdirom-dist@hotmail.com")]
     public class WorksheetsController : Controller
     {
-        
+
         private WsRepository _repository = new WsRepository();
         private ContextSR db = new ContextSR();
 
@@ -27,7 +27,7 @@ namespace SistemaRentabilidad.Controllers
         {
             IList<WsVM> WsVMList = new List<WsVM>();
             IList<Worksheet> WsList = _repository.FindAllE();
-            WsVMList = Mapper.Map<IList<Worksheet>, IList<WsVM>>(WsList);            
+            WsVMList = Mapper.Map<IList<Worksheet>, IList<WsVM>>(WsList);
             return View(WsVMList);
         }
 
@@ -61,7 +61,7 @@ namespace SistemaRentabilidad.Controllers
         }
         public ActionResult DetailsLast()
         {
-            var id=db.Worksheet.ToList().LastOrDefault().IdWorksheet;
+            var id = db.Worksheet.ToList().LastOrDefault().IdWorksheet;
 
             return RedirectToAction("Details", new { id = id });
         }
@@ -79,11 +79,40 @@ namespace SistemaRentabilidad.Controllers
             }
 
             List<string> lines = new List<string>();
+            List<string> linesi = new List<string>();
+            List<string> linesc = new List<string>();
+            List<string> linesg = new List<string>();
+            List<string> lineso = new List<string>();
+
             foreach (var x in db.SheetLines)
             {
+                if (x.SheetType == SheetType.Ingreso)
+                { linesi.Add(x.LineDescription); }
+                else
+                {
+                    if (x.SheetType == SheetType.Costo)
+                    { linesc.Add(x.LineDescription); }
+                    else
+                    {
+                        if (x.SheetType == SheetType.Gasto)
+                        { linesg.Add(x.LineDescription); }
+                        else
+                        {
+                            if (x.SheetType == SheetType.OtroIngreso)
+                            { lineso.Add(x.LineDescription); }
+                        }
+                    }
+
+                }
+
                 lines.Add(x.LineDescription);
             }
+
             ViewBag.lines = JsonConvert.SerializeObject(lines);
+            ViewBag.linesi = JsonConvert.SerializeObject(linesi);
+            ViewBag.linesc = JsonConvert.SerializeObject(linesc);
+            ViewBag.linesg = JsonConvert.SerializeObject(linesg);
+            ViewBag.lineso = JsonConvert.SerializeObject(lineso);
 
             return View(wsVm);
         }
@@ -93,29 +122,29 @@ namespace SistemaRentabilidad.Controllers
         {
             //CustomerName contiene el id del cliente
             bool status = false;
-            
+
             Worksheet wx = db.Worksheet.Find(O.IdWorksheet);
             var existe = db.Worksheet.ToList().Exists(f => f.Date.Year == O.Date.Year & f.Date.Month == O.Date.Month & f.IdWorksheet != O.IdWorksheet);
             if (existe) { return new JsonResult { Data = new { status = status, mensaje = "Ya existe una planilla con ese periodo." } }; }
 
-            List<int> sheetsid=new List<int>();
+            List<int> sheetsid = new List<int>();
             foreach (var item in wx.Sheets.ToList())
             {
                 sheetsid.Add(item.IdSheet);
             }
             foreach (var elim in sheetsid)
             {
-                Sheet sheet=new Sheet();
+                Sheet sheet = new Sheet();
                 sheet = db.Sheet.Find(elim);
                 db.Sheet.Remove(sheet);
                 db.SaveChanges();
-                
+
             }
-           
+
             Worksheet ws = db.Worksheet.Find(O.IdWorksheet);
             if (ModelState.IsValid)
             {
-               
+
                 ws.Date = O.Date;
                 ws.Comments = O.Comments;
                 ws.TotalAmount = O.TotalAmount;
@@ -145,7 +174,7 @@ namespace SistemaRentabilidad.Controllers
             {
                 status = false;
             }
-            return new JsonResult { Data = new { status = status,num=ws.IdWorksheet } };
+            return new JsonResult { Data = new { status = status, num = ws.IdWorksheet } };
         }
 
         public ActionResult HelpCreate()
@@ -168,14 +197,41 @@ namespace SistemaRentabilidad.Controllers
                 nsheet = db.Worksheet.ToList().LastOrDefault().IdWorksheet;
             }
             List<string> lines = new List<string>();
+            List<string> linesi = new List<string>();
+            List<string> linesc = new List<string>();
+            List<string> linesg = new List<string>();
+            List<string> lineso = new List<string>();
 
             foreach (var x in db.SheetLines)
             {
+                if (x.SheetType == SheetType.Ingreso)
+                { linesi.Add(x.LineDescription); }
+                else
+                {
+                    if (x.SheetType == SheetType.Costo)
+                    { linesc.Add(x.LineDescription); }
+                    else
+                    {
+                        if (x.SheetType == SheetType.Gasto)
+                        { linesg.Add(x.LineDescription); }
+                        else
+                        {
+                            if (x.SheetType == SheetType.OtroIngreso)
+                            { lineso.Add(x.LineDescription); }
+                        }
+                    }
+
+                }
+
                 lines.Add(x.LineDescription);
             }
 
             ViewBag.nsheet = nsheet + 1;
             ViewBag.lines = JsonConvert.SerializeObject(lines);
+            ViewBag.linesi = JsonConvert.SerializeObject(linesi);
+            ViewBag.linesc = JsonConvert.SerializeObject(linesc);
+            ViewBag.linesg = JsonConvert.SerializeObject(linesg);
+            ViewBag.lineso = JsonConvert.SerializeObject(lineso);
             return View();
         }
 
@@ -185,10 +241,10 @@ namespace SistemaRentabilidad.Controllers
             //CustomerName contiene el id del cliente
             bool status = false;
             var existe = db.Worksheet.ToList().Exists(f => f.Date.Year == O.Date.Year & f.Date.Month == O.Date.Month);
-            if (existe) { return new JsonResult { Data = new { status = status,mensaje="Ya existe una planilla con ese periodo." } }; }
+            if (existe) { return new JsonResult { Data = new { status = status, mensaje = "Ya existe una planilla con ese periodo." } }; }
 
             Worksheet ws = new Worksheet();
-           
+
             if (ModelState.IsValid)
             {
 
@@ -206,10 +262,11 @@ namespace SistemaRentabilidad.Controllers
 
                 foreach (var i in O.Sheets)
                 {
-                    var exline = db.SheetLines.ToList().Exists(f => f.LineDescription == i.SheetDescription & f.SheetType== i.SheetType);
+                    var exline = db.SheetLines.ToList().Exists(f => f.LineDescription == i.SheetDescription & f.SheetType == i.SheetType);
 
                     if (!exline)
-                    {var newline=new SheetLine();
+                    {
+                        var newline = new SheetLine();
                         newline.LineDescription = i.SheetDescription;
                         newline.SheetType = i.SheetType;
                         db.SheetLines.Add(newline);
@@ -239,28 +296,7 @@ namespace SistemaRentabilidad.Controllers
         }
 
 
-        // GET: Worksheets/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
 
-        // POST: Worksheets/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "IdWorksheet,Date,TotalAmount,Comments")] WsVM wsVm)
-        {
-            if (ModelState.IsValid)
-            {
-                Worksheet worksheet = Mapper.Map<WsVM, Worksheet>(wsVm);
-                _repository.AddE(worksheet);
-                return RedirectToAction("Index");
-            }
-
-            return View(wsVm);
-        }
         public JsonResult GetLines()
         {
 
@@ -275,7 +311,7 @@ namespace SistemaRentabilidad.Controllers
         }
         public JsonResult ExistePlanilla(DateTime fecha)
         {
-            
+
             var existe = db.Worksheet.ToList().Exists(f => f.Date.Year == fecha.Year & f.Date.Month == fecha.Month);
 
             return Json(existe, JsonRequestBehavior.AllowGet);
@@ -283,42 +319,13 @@ namespace SistemaRentabilidad.Controllers
         public JsonResult ExistePlanillaEdit(DateTime fecha, int idp)
         {
 
-            var existe = db.Worksheet.ToList().Exists(f => f.Date.Year == fecha.Year & f.Date.Month == fecha.Month &f.IdWorksheet!=idp);
+            var existe = db.Worksheet.ToList().Exists(f => f.Date.Year == fecha.Year & f.Date.Month == fecha.Month & f.IdWorksheet != idp);
 
             return Json(existe, JsonRequestBehavior.AllowGet);
         }
 
-        // GET: Worksheets/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Worksheet worksheet = _repository.FindE(id);
-            WsVM wsVm = Mapper.Map<Worksheet, WsVM>(worksheet);
-            if (worksheet == null)
-            {
-                return HttpNotFound();
-            }
-            return View(wsVm);
-        }
 
-        // POST: Worksheets/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "IdWorksheet,Date,TotalAmount,Comments")] WsVM wsVm)
-        {
-            if (ModelState.IsValid)
-            {
-                Worksheet worksheet = Mapper.Map<WsVM, Worksheet>(wsVm);
-                _repository.EditE(worksheet);
-                return RedirectToAction("Index");
-            }
-            return View(wsVm);
-        }
+
 
         // GET: Worksheets/Delete/5
         public ActionResult Delete(int? id, bool details)
@@ -343,7 +350,7 @@ namespace SistemaRentabilidad.Controllers
                 return View("DeleteIndex", wsVm);
             }
 
-            
+
         }
 
         // POST: Worksheets/Delete/5
@@ -360,7 +367,7 @@ namespace SistemaRentabilidad.Controllers
         {
             if (disposing)
             {
-               
+
             }
             base.Dispose(disposing);
         }
