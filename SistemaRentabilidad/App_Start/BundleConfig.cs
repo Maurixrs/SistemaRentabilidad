bda2d3ddd549dfb0f4bdb56f6d0ebbfb00f7823c
mln6666 <poncemarioleo@gmail.com>
2017-02-25 03:17:00 -0300

    +Configuraciones para producción

--- SistemaRentabilidad/App_Start/BundleConfig.cs
+++ SistemaRentabilidad/App_Start/BundleConfig.cs
@@ -8,8 +8,11 @@ namespace SistemaRentabilidad
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
-                         "~/Scripts/jquery-1.12.4.js",
+            BundleTable.EnableOptimizations = true;
+            ScriptBundle scriptBundle = new ScriptBundle("~/js");
+            string[] scriptArray =
+            {
+             "~/Scripts/jquery-1.12.4.js",
                        "~/Scripts/jquery-1.12.4.min.js",
                         "~/Scripts/jquery-ui-1.12.1.js",
                        "~/Scripts/jquery-ui-1.12.1.min.js",
@@ -27,10 +30,37 @@ namespace SistemaRentabilidad
                        "~/Scripts/autosize.js",
                        "~/Scripts/bootstrap-select.min.js", "~/Scripts/awesomplete.js",
                        "~/Scripts/maxlength.js",
-                       "~/Scripts/i18n/defaults-ar_AR.min.js", "~/Scripts/Monthpicker.js"
+                       "~/Scripts/i18n/defaults-ar_AR.min.js"
+        };
+            scriptBundle.Include(scriptArray);
+            scriptBundle.IncludeDirectory("~/Scripts/", "*.js");
+            bundles.Add(scriptBundle);
+
+          
+
+//            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
+//                         "~/Scripts/jquery-1.12.4.js",
+//                       "~/Scripts/jquery-1.12.4.min.js",
+//                        "~/Scripts/jquery-ui-1.12.1.js",
+//                       "~/Scripts/jquery-ui-1.12.1.min.js",
+//                        "~/Scripts/jquery.unobtrusive-ajax.js",
+//                       "~/Scripts/bootstrap.js",
+//                       "~/Scripts/datatables/jquery.datatables.js",
+//                       "~/Scripts/datatables/datatables.bootstrap.js",
+//                       "~/Scripts/bootstrap.min.js",
+//                       "~/Scripts/respond.js",
+//                       "~/Scripts/bootbox.js",
+//                       "~/Scripts/toastr.js",
+//                       "~/Scripts/ordenFechas.js",
+//                       "~/Scripts/cargar.js",
+//                        "~/Scripts/jquery.unobtrusive-ajax.js",
+//                       "~/Scripts/autosize.js",
+//                       "~/Scripts/bootstrap-select.min.js", "~/Scripts/awesomplete.js",
+//                       "~/Scripts/maxlength.js",
+//                       "~/Scripts/i18n/defaults-ar_AR.min.js", "~/Scripts/Monthpicker.js"
 
 
-));
+//));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -41,13 +71,22 @@ namespace SistemaRentabilidad
                         "~/Scripts/modernizr-*"));
 
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap-flaty.css", "~/Content/css/awesomplete.css",
+
+            bundles.Add(new StyleBundle("~/Styles/css").Include(
+            "~/Content/bootstrap-flaty.css", "~/Content/css/awesomplete.css",
                       "~/Content/bootstrap-select.css",
                       "~/Content/bootstrap-select.css.map",
                       "~/Content/datatables/css/datatables.bootstrap.css",
                       "~/content/toastr.css",
-                      "~/Content/site.css", "~/Content/css/jquery-ui.min.css", "~/Content/Monthpicker.css"));
+                       "~/Content/css/jquery-ui.min.css", "~/Content/Site.css"));
+
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap-flaty.css", "~/Content/css/awesomplete.css",
+            //          "~/Content/bootstrap-select.css",
+            //          "~/Content/bootstrap-select.css.map",
+            //          "~/Content/datatables/css/datatables.bootstrap.css",
+            //          "~/content/toastr.css",
+            //          "~/Content/site.css", "~/Content/css/jquery-ui.min.css", "~/Content/Monthpicker.css"));
         }
     }
 }
